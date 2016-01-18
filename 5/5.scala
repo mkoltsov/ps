aa7ef41d818394855b201936ca4693edace7206d
mkoltsov <root@javabean.ru>
2016-01-18 23:38:01 +0000

    5 completed

---
+++ 5/5.scala
@@ -0,0 +1,14 @@
+val sym = 'aSymbol
+
+println(sym.getClass())
+
+println(sym.name)
+
+println( 0 max 5 )
+println( 0 min 5 )
+println( -2.7 abs )
+println( 1.5 isInfinity )
+println( (1.0/0) isInfinity )
+println( (4 to 6) )
+println( "Bob" capitalize )
+println( "Robert" drop 2 )
