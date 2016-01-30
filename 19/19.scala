1b6b2f9d5cddfb9cba0df05a2dd51f430be7472f
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-01-30 16:29:28 +0100

    19 finished

--- 19/19.scala
+++ 19/19.scala
@@ -1,4 +1,4 @@
- class Queue[T](
+ class Queue[+T](
     private val leading: List[T],
     private val trailing: List[T]
 ){
@@ -11,8 +11,8 @@ else this
       val q = mirror
       new Queue(q.leading.tail, q.trailing)
 }
-    def enqueue(x: T) =
-      new Queue(leading, x :: trailing)
+    def enqueue[U>: T](x: U) =
+      new Queue[U](leading, x :: trailing)
     override def toString = s"$leading::$trailing"
 }
 
@@ -37,3 +37,97 @@ val xy = HiddenQueue2(List("chef","pupa"))
 val x = new Queue(List(1,3,4), Nil)
 
 println(x.enqueue(8))
+
+trait Queue1[T] {
+  def head: T
+  def tail: Queue1[T]
+  def enqueue(x: T): Queue1[T]
+}
+object Queue1 {
+  def apply[T](xs: T*): Queue1[T] =
+    new QueueImpl[T](xs.toList, Nil)
+  private class QueueImpl[T](
+    private val leading: List[T],
+    private val trailing: List[T]
+  ) extends Queue1[T] {
+    def mirror =
+      if (leading.isEmpty)
+        new QueueImpl(trailing.reverse, Nil)
+      else
+this
+    def head: T = mirror.leading.head
+    def tail: QueueImpl[T] = {
+      val q = mirror
+      new QueueImpl(q.leading.tail, q.trailing)
+}
+    def enqueue(x: T) =
+      new QueueImpl(leading, x :: trailing)
+} }
+
+val f = Queue1(List(1,2,3))
+
+def doesCompile(d:Queue1[AnyRef]) = println(d)
+
+//doesCompile(f)
+// The new definition gives enqueue a type parameter U, and with the syntax, “U >: T”,
+ // defines T as the lower bound for U. As a result, U is required to be a supertype of T
+class Cell[T](init: T) {
+    private[this] var current = init
+    def get = current
+    def set(x: T) { current = x }
+}
+
+trait OutputChannel[-T] {
+  def write(x:T)
+}
+
+class SomeChannel[U] extends OutputChannel[U] {
+  def write(x:U) = println(x)
+}
+
+val fg = new SomeChannel[AnyRef]
+
+fg.write("fdf")
+fg.write(new Object)
+// fg.write(new Object)
+
+class Person(val firstName: String, val lastName: String)
+            extends Ordered[Person] {
+          def compare(that: Person) = {
+            val lastNameComparison =
+              lastName.compareToIgnoreCase(that.lastName)
+            if (lastNameComparison != 0)
+              lastNameComparison
+else
+              firstName.compareToIgnoreCase(that.firstName)
+          }
+          override def toString = firstName +" "+ lastName
+        }
+// With the “T <: Ordered[T]” syntax, you indicate that the type parameter, T, has an upper bound, Ordered[T].
+// This means that the element type of the list passed to orderedMergeSort must be a subtype of Ordered
+def orderedMergeSort[T <: Ordered[T]](xs: List[T]): List[T] = {
+        def merge(xs: List[T], ys: List[T]): List[T] =
+          (xs, ys) match {
+            case (Nil, _) => ys
+            case (_, Nil) => xs
+            case (x :: xs1, y :: ys1) =>
+              if (x < y) x :: merge(xs1, ys)
+              else y :: merge(xs, ys1)
+          }
+        val n = xs.length / 2
+        if (n == 0) xs
+        else {
+          val (ys, zs) = xs splitAt n
+          merge(orderedMergeSort(ys), orderedMergeSort(zs))
+        }
+}
+
+val people = List(
+                 new Person("Larry", "Wall"),
+                 new Person("Anders", "Hejlsberg"),
+                 new Person("Guido", "van Rossum"),
+                 new Person("Alan", "Kay"),
+                 new Person("Yukihiro", "Matsumoto")
+)
+
+println(orderedMergeSort(people))        
