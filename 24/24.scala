ba52c8e0a85b136a330df6e01b7cd3104ce94dff
mkoltsov <root@javabean.ru>
2016-02-05 00:13:46 +0000

    24 started

--- 24/24.scala
+++ 24/24.scala
@@ -1,3 +1,7 @@
-val l = List(1,2,3,4).partition(_<3)
+val li = List(1,2,3,4)
+val l = li.partition(_<3)
 
 println(l)
+
+println(li.flatMap{case 3 => "!"; case _ => ""})
+println(li.map{case 3 => "!"; case _ => ""})
